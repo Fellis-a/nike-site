b756ab97fc7c1225b50c73a061e7d7b5c0b13bb3
Alisa Fedorova <2012alis@mail.ru>
2024-01-11 13:03:37 +0300

    Changes in modal js

--- js/cart.js
+++ js/cart.js
@@ -1,19 +1,19 @@
-var addToCartButtons = document.getElementsByClassName('add-to-cart');
-var removeFromCartButtons = document.getElementsByClassName('remove-from-cart-btn');
-var cartCounter = 0;
-var cartTotal = 0;
+let addToCartButtons = document.getElementsByClassName('add-to-cart');
+let removeFromCartButtons = document.getElementsByClassName('remove-from-cart-btn');
+let cartCounter = 0;
+let cartTotal = 0;
 let quantity = 1;
 
 
 
 //навешиваем обработчики событий на кнопки добавления товаров в корзину
-for (var i = 0; i < addToCartButtons.length; i++) {
+for (let i = 0; i < addToCartButtons.length; i++) {
     addToCartButtons[i].addEventListener('click', function () {
-        var product = this.parentNode;
-        var productName = product.getElementsByClassName('product_name')[0].textContent;
+        let product = this.parentNode;
+        let productName = product.getElementsByClassName('product_name')[0].textContent;
 
-        var priceElement = product.getElementsByClassName('product_price')[0];
-        var price = parseFloat(priceElement.textContent);
+        let priceElement = product.getElementsByClassName('product_price')[0];
+        let price = parseFloat(priceElement.textContent);
 
 
         addToCart(productName, price);
@@ -27,12 +27,12 @@ for (var i = 0; i < addToCartButtons.length; i++) {
 }
 
 //навешиваем обработчики событий на кнопки удаления товаров в корзину
-for (var i = 0; i < removeFromCartButtons.length; i++) {
+for (let i = 0; i < removeFromCartButtons.length; i++) {
     removeFromCartButtons[i].addEventListener('click', function () {
-        var product = this.parentNode;
-        var productName = product.getElementsByClassName('product_name')[0].textContent;
-        var priceElement = product.getElementsByClassName('product_price')[0];
-        var price = parseFloat(priceElement.textContent);
+        let product = this.parentNode;
+        let productName = product.getElementsByClassName('product_name')[0].textContent;
+        let priceElement = product.getElementsByClassName('product_price')[0];
+        let price = parseFloat(priceElement.textContent);
 
         removeFromCart(productName, price);
         updateCartCounter();
@@ -45,9 +45,9 @@ for (var i = 0; i < removeFromCartButtons.length; i++) {
 
 // Функция добавления товаров в корзину 
 function addToCart(productName, price) {
-    var cartItems = document.getElementById('cart-items');
-    var listItem = document.createElement('li');
-    var itemContainer = document.createElement('div');
+    let cartItems = document.getElementById('cart-items');
+    let listItem = document.createElement('li');
+    let itemContainer = document.createElement('div');
     itemContainer.innerHTML = `${productName} x ${quantity} - ${price * quantity} руб. <span class="remove-from-cart-btn close-danger">&times;</span>`;
     listItem.appendChild(itemContainer);
 
@@ -55,7 +55,7 @@ function addToCart(productName, price) {
     cartItems.appendChild(listItem);
 
     // Обработчик событий для кнопки удаления товаров в корзие (рядом с товарами)
-    var deleteButton = listItem.querySelector('.remove-from-cart-btn');
+    let deleteButton = listItem.querySelector('.remove-from-cart-btn');
     deleteButton.addEventListener('click', function () {
 
         if (confirm('Удалить?')) {
@@ -78,11 +78,11 @@ function addToCart(productName, price) {
 
 //удаление товаров в корзине
 function removeFromCart(productName, price) {
-    var cartItems = document.getElementById('cart-items');
-    var items = cartItems.getElementsByTagName('li');
+    let cartItems = document.getElementById('cart-items');
+    let items = cartItems.getElementsByTagName('li');
 
-    for (var i = items.length - 1; i >= 0; i--) {
-        var itemText = items[i].textContent;
+    for (let i = items.length - 1; i >= 0; i--) {
+        let itemText = items[i].textContent;
         if (itemText.includes(productName)) {
             cartItems.removeChild(items[i]);
 
@@ -96,19 +96,19 @@ function removeFromCart(productName, price) {
 
 //обновление значения счетчика товаров на сайте 
 function updateCartCounter() {
-    var cartItems = document.getElementById('cart-items');
-    var items = cartItems.getElementsByTagName('li');
+    let cartItems = document.getElementById('cart-items');
+    let items = cartItems.getElementsByTagName('li');
     cartCounter = items.length;
 
-    var cartCounterElement = document.getElementById('cart-counter');
+    let cartCounterElement = document.getElementById('cart-counter');
     cartCounterElement.textContent = cartCounter;
 }
 
 //обработчик для кнопки очистки корзины
 
-var clearCartButton = document.getElementById('clear-cart-btn');
+let clearCartButton = document.getElementById('clear-cart-btn');
 clearCartButton.addEventListener('click', function () {
-    var cartItems = document.getElementById('cart-items');
+    let cartItems = document.getElementById('cart-items');
     cartItems.innerHTML = '';
     cartCounter = 0;
     cartTotal = 0;
@@ -116,7 +116,7 @@ clearCartButton.addEventListener('click', function () {
     updateCartTotal();
 
     //смена кнопок "удалить" на "в корзину" в карточках товаров при очистке корзины
-    for (var i = 0; i < removeFromCartButtons.length; i++) {
+    for (let i = 0; i < removeFromCartButtons.length; i++) {
         removeFromCartButtons[i].style.display = 'none';
         addToCartButtons[i].style.display = 'inline-block';
     }
@@ -124,20 +124,20 @@ clearCartButton.addEventListener('click', function () {
 
 //Функция для показа итоговой суммы за товары в корзине
 function updateCartTotal() {
-    var cartTotalElement = document.getElementById('cart-total');
+    let cartTotalElement = document.getElementById('cart-total');
     cartTotalElement.textContent = cartTotal;
 }
 
 //Функция для смены кнопок на основной странице при удалении товаров из корзины
 
 function updateProductCardButtons(productName, showAddButton) {
-    var productCards = document.getElementsByClassName('product');
-    for (var i = 0; i < productCards.length; i++) {
-        var cardProductName = productCards[i].getElementsByClassName('product_name')[0].textContent;
+    let productCards = document.getElementsByClassName('product');
+    for (let i = 0; i < productCards.length; i++) {
+        let cardProductName = productCards[i].getElementsByClassName('product_name')[0].textContent;
 
         if (cardProductName === productName) {
-            var addToCartButton = productCards[i].getElementsByClassName('add-to-cart')[0];
-            var removeFromCartButton = productCards[i].getElementsByClassName('remove-from-cart-btn')[0];
+            let addToCartButton = productCards[i].getElementsByClassName('add-to-cart')[0];
+            let removeFromCartButton = productCards[i].getElementsByClassName('remove-from-cart-btn')[0];
 
             if (showAddButton) {
                 addToCartButton.style.display = 'inline-block';
--- js/main.js
+++ js/main.js
@@ -1,44 +1,59 @@
-var stars = new StarRating('.star-rating',);
+let stars = new StarRating('.star-rating',);
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+
 function openModal() {
-    let modal = document.getElementById('myModalLogin');
+    let modal = document.getElementById("myModalLogin");
     const body = document.querySelector('body');
+
+    let span = document.getElementsByClassName("close")[0];
+
 
     body.style.overflow = 'hidden';
-    modal.style.display = 'block';
-}
+    modal.style.display = "block";
 
-function closeModal() {
-    let modal = document.getElementById('myModalLogin');
+    span.onclick = function () {
+        modal.style.display = "none";
+        body.style.overflow = 'auto';
+    }
 
-
-    const body = document.querySelector('body');
-    modal.style.display = 'none';
-
-    body.style.overflow = 'auto';
-
+    window.onclick = function (event) {
+        //если мы кликнули вне модального окна, но в окне браузера
+        //то белое модальное окно исчезает
+        //display:none
+        if (event.target == modal) {
+            modal.style.display = "none";
+            body.style.overflow = 'auto';
+        }
+    }
 }
 
 function openModal1() {
-    let modal = document.getElementById('myModal1');
+    let modal = document.getElementById("myModal1");
     const body = document.querySelector('body');
+
+    let span = document.getElementsByClassName("close")[1];
+
 
     body.style.overflow = 'hidden';
-    modal.style.display = 'block';
-}
+    modal.style.display = "block";
 
-function closeModal1() {
-    let modal = document.getElementById('myModal1');
+    span.onclick = function () {
+        modal.style.display = "none";
+        body.style.overflow = 'auto';
+    }
 
-
-    const body = document.querySelector('body');
-    modal.style.display = 'none';
-
-    body.style.overflow = 'auto';
-
+    window.onclick = function (event) {
+        //если мы кликнули вне модального окна, но в окне браузера
+        //то белое модальное окно исчезает
+        //display:none
+        if (event.target == modal) {
+            modal.style.display = "none";
+            body.style.overflow = 'auto';
+        }
+    }
 }
 
 
@@ -68,13 +83,13 @@ document.querySelectorAll(".header_menu-item").forEach(n => n.addEventListener("
     });
 }))
 
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
 });
-var swiper2 = new Swiper(".mySwiper2", {
+let swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     navigation: {
         nextEl: ".swiper-button-next",
@@ -86,7 +101,7 @@ var swiper2 = new Swiper(".mySwiper2", {
 });
 
 
-var lightbox = new SimpleLightbox('.gallery a', {
+let lightbox = new SimpleLightbox('.gallery a', {
     zoom: true,
     alertError: true,
     alertErrorMessage: "",
